d86be67d3c67c2be6bc46ab0d1011f1f8473d3e8
alexrodriguesbash <alek.rodrigues@hotmail.com>
2023-10-11 11:47:29 -0300

    Desafio Controle de Fluxo (DIO)

---
+++ src/controle_fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package controle_fluxo;
+
+public class ParametrosInvalidosException extends Exception{
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String errorMessage) {
+		 super(errorMessage);
+	 }
+}
